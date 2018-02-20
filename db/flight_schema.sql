4238044433f178b48bc29ed15a537b3e440c7dba
Lekha Ajith Kutty <ss.lekha@gmail.com>
2018-02-20 19:34:18 +0200

    added method to create tables

--- db/flight_schema.sql
+++ db/flight_schema.sql
@@ -1,7 +1,7 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
-CREATE TABLE  User  (
+CREATE TABLE IF NOT EXISTS Users(
      user_id    INTEGER NOT NULL UNIQUE,
      lastName   TEXT,
      firstName  TEXT,
@@ -10,54 +10,48 @@ CREATE TABLE  User  (
      birthDate  TEXT,
      gender     TEXT,
      registrationDate   TEXT,
-    PRIMARY KEY( user_id )
-);
+    PRIMARY KEY(user_id ));
 
-CREATE TABLE  Reservation  (
-	 reservation_id 	INTEGER NOT NULL UNIQUE,
-	 reference 	TEXT,
-	 re_date 	TEXT,
-	 creator_id 	INTEGER NOT NULL,
-	 flight_id 	INTEGER NOT NULL,
-	 ticket_id 	INTEGER NOT NULL,
-	PRIMARY KEY( reservation_id ),
-	FOREIGN KEY( flight_id ) REFERENCES  Flight ( flight_id ) ON DELETE CASCADE,
-	FOREIGN KEY( ticket_id ) REFERENCES  Ticket ( ticket_id ) ON DELETE CASCADE,
-	FOREIGN KEY( creator_id ) REFERENCES  User ( user_id ) ON DELETE CASCADE,
-	
-);
+CREATE TABLE IF NOT EXISTS Reservation(
+     reservation_id     INTEGER NOT NULL UNIQUE,
+     reference  TEXT,
+     re_date    TEXT,
+     creator_id     INTEGER NOT NULL,
+     flight_id  INTEGER NOT NULL,
+     ticket_id  INTEGER NOT NULL,
+    PRIMARY KEY( reservation_id ),
+    FOREIGN KEY( flight_id ) REFERENCES  Flight ( flight_id ) ON DELETE CASCADE,
+    FOREIGN KEY( ticket_id ) REFERENCES  Ticket ( ticket_id ) ON DELETE CASCADE,
+    FOREIGN KEY( creator_id ) REFERENCES  Users ( user_id ) ON DELETE CASCADE);
 
-CREATE TABLE  Ticket  (
-	 ticket_id 	INTEGER NOT NULL UNIQUE,
-	 fullName 	TEXT,
-	 age 	INTEGER,
-	 reservation_id 	INTEGER NOT NULL,
-	 price 	INTEGER,
-	 seat 	TEXT,
-	PRIMARY KEY( ticket_id ),
-	FOREIGN KEY( reservation_id ) REFERENCES  Reservation ( reservation_id ) ON DELETE CASCADE
-);
+CREATE TABLE IF NOT EXISTS Ticket(
+     ticket_id  INTEGER NOT NULL UNIQUE,
+     fullName   TEXT,
+     age    INTEGER,
+     reservation_id     INTEGER NOT NULL,
+     price  INTEGER,
+     seat   TEXT,
+    PRIMARY KEY( ticket_id ),
+    FOREIGN KEY( reservation_id ) REFERENCES  Reservation ( reservation_id ) ON DELETE CASCADE);
 
-CREATE TABLE  Flight  (
-	 flight_id 	INTEGER NOT NULL UNIQUE,
-	 code 	TEXT UNIQUE,
-	 gate 	TEXT,
-	 depDate 	TEXT,
-	 nbInitialSeats 	INTEGER,
-	 nbSeatsLeft 	INTEGER,
-	 template_id 	INTEGER NOT NULL,
-	FOREIGN KEY( template_id ) REFERENCES  TemplateFlights ( tflight_id ) ON DELETE CASCADE,
-	PRIMARY KEY( flight_id )
-);
+CREATE TABLE IF NOT EXISTS Flight(
+     flight_id  INTEGER NOT NULL UNIQUE,
+     code   TEXT UNIQUE,
+     gate   TEXT,
+     depDate    TEXT,
+     nbInitialSeats     INTEGER,
+     nbSeatsLeft    INTEGER,
+     template_id    INTEGER NOT NULL,
+    FOREIGN KEY( template_id ) REFERENCES  TemplateFlights ( tflight_id ) ON DELETE CASCADE,
+    PRIMARY KEY( flight_id ));
 
-CREATE TABLE  TemplateFlights  (
-	 tflight_id 	INTEGER NOT NULL UNIQUE,
-	 depTime 	TEXT,
-	 arrTime 	TEXT,
-	 origin 	TEXT,
-	 destination 	TEXT,
-	PRIMARY KEY( tflight_id )
-);
+CREATE TABLE IF NOT EXISTS TemplateFlights(
+     tflight_id     INTEGER NOT NULL UNIQUE,
+     depTime    TEXT,
+     arrTime    TEXT,
+     origin     TEXT,
+     destination    TEXT,
+    PRIMARY KEY( tflight_id ));
 
 COMMIT;
 PRAGMA foreign_keys=ON;
--- flight_reservation/flight_databse.py
+++ flight_reservation/flight_databse.py
@@ -4,17 +4,22 @@ Provides the database API to access the forum persistent data.
 
 '''
 from datetime import datetime
-import time, sqlite3, re, os
+import time, sqlite3, os
 #Default paths for .db and .sql files to create and populate the database.
 DEFAULT_DB_PATH = '../db/flight.db'
 DEFAULT_SCHEMA = "../db/flight_schema.sql"
 
-conn = sqlite3.connect("flight.db")
-c = conn.cursor()
 
 
+def create_tables(db_path=DEFAULT_DB_PATH, schema_path=DEFAULT_SCHEMA):
+	conn = sqlite3.connect(db_path)
+	c = conn.cursor()
+	try:
+		with open(schema_path, encoding="utf-8") as f:
+			sql = f.read()
+			c.executescript(sql)
+			conn.commit()
+	finally:
+		conn.close()
 
-
-
-conn.commit()
-conn.close()
+create_tables()
