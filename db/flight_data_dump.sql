505d499a6430165e45e4b20c80469a4c7f12f73c
Jules <jules.larue@outlook.fr>
2018-02-21 21:03:14 +0200

    fixed order of SQL insert statements

--- db/flight_data_dump.sql
+++ db/flight_data_dump.sql
@@ -1,27 +1,3 @@
-INSERT INTO "User" VALUES(1,'Tilton','John',92722736387,'john.tilton@jhj.jh',04041981,'male',20022018);
-INSERT INTO "User" VALUES(2,'Sam','Jacob',927656756,'sam.jacob@jhj.jh',05041981,'male',20022018);
-INSERT INTO "User" VALUES(3,'Simon','Johni',895454587,'simon.johni@jhj.jh',04071989,'male',20022018);
-INSERT INTO "User" VALUES(4,'Mike','Jac',89766736387,'mic.jac@jhj.jh',09011985,'male',20022018);
-INSERT INTO "User" VALUES(5,'Niil','Jain',454555667,'niil.jain@jhj.jh',03041983,'male',20022018);
-
-INSERT INTO "Reservation" VALUES(11,'AB12CS',20022018,1,1111);
-INSERT INTO "Reservation" VALUES(22,'HJJJHW',29022018,2,1122);
-INSERT INTO "Reservation" VALUES(33,'ABGJHG',09022018,3,1133);
-INSERT INTO "Reservation" VALUES(44,'OIOYHA',30082018,5,1133);
-
-INSERT INTO "Ticket" VALUES(1010,'John','Tilton','male',20,11,'21A');
-INSERT INTO "Ticket" VALUES(1020,'Jacob','Tilton','male',25,11,'21B');
-INSERT INTO "Ticket" VALUES(1030,'Sam','Jacob','male',29,22,'1A');
-INSERT INTO "Ticket" VALUES(1040,'Molly','Jacob','female',40,22,'1B');
-INSERT INTO "Ticket" VALUES(1050,'Niil','Jain','male',29,44,'15A');
-
-
-
-INSERT INTO "Flight" VALUES(1111,'AY101',200,'GATE02',05062018,07062018,90,10,1234);
-INSERT INTO "Flight" VALUES(1122,'AY201',150,'GATE04',10062018,20062018,90,15,1235);
-INSERT INTO "Flight" VALUES(1133,'AY523',180,'GATE01',05072018,07072018,90,20,1237);
-
-
 INSERT INTO "TemplateFlight" VALUES(1234,2052,2340,'Finland','France');
 INSERT INTO "TemplateFlight" VALUES(1235,1050,1310,'Finland','Spain');
 INSERT INTO "TemplateFlight" VALUES(1236,0730,0910,'Finland','Sweeden');
@@ -29,5 +5,26 @@ INSERT INTO "TemplateFlight" VALUES(1237,0400,0600,'Finland','Berlin');
 INSERT INTO "TemplateFlight" VALUES(1238,2052,2340,'Finland','Poland');
 
 
+INSERT INTO "Flight" VALUES(1111,'AY101',200,'GATE02',05062018,07062018,90,10,1234);
+INSERT INTO "Flight" VALUES(1122,'AY201',150,'GATE04',10062018,20062018,90,15,1235);
+INSERT INTO "Flight" VALUES(1133,'AY523',180,'GATE01',05072018,07072018,90,20,1237);
 
 
+INSERT INTO "User" VALUES(1,'Tilton','John',92722736387,'john.tilton@jhj.jh',04041981,'male',20022018);
+INSERT INTO "User" VALUES(2,'Sam','Jacob',927656756,'sam.jacob@jhj.jh',05041981,'male',20022018);
+INSERT INTO "User" VALUES(3,'Simon','Johni',895454587,'simon.johni@jhj.jh',04071989,'male',20022018);
+INSERT INTO "User" VALUES(4,'Mike','Jac',89766736387,'mic.jac@jhj.jh',09011985,'male',20022018);
+INSERT INTO "User" VALUES(5,'Niil','Jain',454555667,'niil.jain@jhj.jh',03041983,'male',20022018);
+
+
+INSERT INTO "Reservation" VALUES(11,'AB12CS',20022018,1,1111);
+INSERT INTO "Reservation" VALUES(22,'HJJJHW',29022018,2,1122);
+INSERT INTO "Reservation" VALUES(33,'ABGJHG',09022018,3,1133);
+INSERT INTO "Reservation" VALUES(44,'OIOYHA',30082018,5,1133);
+
+
+INSERT INTO "Ticket" VALUES(1010,'John','Tilton','male',20,11,'21A');
+INSERT INTO "Ticket" VALUES(1020,'Jacob','Tilton','male',25,11,'21B');
+INSERT INTO "Ticket" VALUES(1030,'Sam','Jacob','male',29,22,'1A');
+INSERT INTO "Ticket" VALUES(1040,'Molly','Jacob','female',40,22,'1B');
+INSERT INTO "Ticket" VALUES(1050,'Niil','Jain','male',29,44,'15A');
