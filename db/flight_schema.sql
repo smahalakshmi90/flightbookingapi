5016cc5560554bde35164d321774a6e9af167d0f
Jules <jules.larue@outlook.fr>
2018-02-25 20:33:01 +0200

    autoincrement tables ids

--- db/flight_schema.sql
+++ db/flight_schema.sql
@@ -2,26 +2,24 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS User(
-     user_id INTEGER NOT NULL UNIQUE,
+     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
      lastName   TEXT,
      firstName  TEXT,
      phoneNumber    TEXT,
      email  TEXT,
      birthDate  TEXT,
      gender     TEXT,
-     registrationDate   INTEGER,
-    PRIMARY KEY(user_id ));
+     registrationDate INTEGER);
 
 CREATE TABLE IF NOT EXISTS TemplateFlight(
-     tflight_id     INTEGER NOT NULL UNIQUE,
+     tflight_id INTEGER PRIMARY KEY AUTOINCREMENT,
      depTime    TEXT,
      arrTime    TEXT,
      origin     TEXT,
-     destination    TEXT,
-    PRIMARY KEY( tflight_id ));
+     destination TEXT);
 
 CREATE TABLE IF NOT EXISTS Flight(
-     flight_id  INTEGER NOT NULL UNIQUE,
+     flight_id  INTEGER PRIMARY KEY AUTOINCREMENT,
      code   TEXT UNIQUE,
      price  INTEGER,
      gate   TEXT,
@@ -30,28 +28,25 @@ CREATE TABLE IF NOT EXISTS Flight(
      nbInitialSeats     INTEGER,
      nbSeatsLeft    INTEGER,
      template_id    INTEGER NOT NULL,
-    FOREIGN KEY( template_id ) REFERENCES  TemplateFlight(tflight_id ) ON DELETE CASCADE,
-    PRIMARY KEY( flight_id ));
+    FOREIGN KEY( template_id ) REFERENCES  TemplateFlight(tflight_id ) ON DELETE CASCADE);
 
 CREATE TABLE IF NOT EXISTS Reservation(
-     reservation_id     INTEGER NOT NULL UNIQUE,
+     reservation_id INTEGER PRIMARY KEY AUTOINCREMENT,
      reference  TEXT,
      re_date    TEXT,
      creator_id     INTEGER NOT NULL,
      flight_id  INTEGER NOT NULL,
-    PRIMARY KEY( reservation_id ),
     FOREIGN KEY( flight_id ) REFERENCES  Flight ( flight_id ) ON DELETE CASCADE,
     FOREIGN KEY( creator_id ) REFERENCES  User( user_id ) ON DELETE CASCADE);
 
 CREATE TABLE IF NOT EXISTS Ticket(
-     ticket_id  INTEGER NOT NULL UNIQUE,
+     ticket_id  INTEGER PRIMARY KEY AUTOINCREMENT,
 	 firstName   TEXT,
 	 lastName   TEXT,
 	 gender   TEXT,
      age    INTEGER,
      reservation_id     INTEGER NOT NULL,
      seat   TEXT,
-    PRIMARY KEY( ticket_id ),
     FOREIGN KEY( reservation_id ) REFERENCES  Reservation ( reservation_id ) ON DELETE CASCADE);
 
 
